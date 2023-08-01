e3a4475b8429a0e6a1cb0ae5945a5d9700781a5a
Clae <56447997+clayruh@users.noreply.github.com>
2023-08-01 15:32:24 -0400

    using props to populate data to site

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props, isBio) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{isBio ? `${props.bio}` : null}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
